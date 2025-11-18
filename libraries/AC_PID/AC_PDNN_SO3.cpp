0b36526adb124a4426eb05035ef7465ce1ab0d38
gao <gao.tianhua1103@gmail.com>
2025-11-18 15:02:41 +0900

    _omega_c_L1 <- 10

--- libraries/AC_PID/AC_PDNN_SO3.cpp
+++ libraries/AC_PID/AC_PDNN_SO3.cpp
@@ -169,9 +169,9 @@ Vector3f AC_PDNN_SO3::update_all(const Matrix3f &R_c, const Matrix3f &R, const V
     //_pdnn_output.y = _J_y * (-_e_R.y * 40.0f - _e_Omega.y * 80.0f - 0.0f *_integrator.y - _geomrtry_output.y- 1.0f * _phi_y + 0.0f*Aug.y);
     //_pdnn_output.z = _J_z * (-_e_R.z * 40.0f - _e_Omega.z * 80.0f - 0.0f *_integrator.z - _geomrtry_output.z - 1.0f *_phi_z + 0.0f*Aug.z); //偏航误差e_R.z很容易就趋近于0，会导致无法满足持续激励假设
     
-    _pdnn_output.x = 0.01f * (-_e_R.x * 40.0f - _e_Omega.x * 40.0f - 1.0f * _integrator.x - _geomrtry_output.x - 0.0f *_phi_x + 0.0f*Aug.x); 
-    _pdnn_output.y = 0.01f * (-_e_R.y * 40.0f - _e_Omega.y * 40.0f - 1.0f *_integrator.y - _geomrtry_output.y- 0.0f * _phi_y + 0.0f*Aug.y);
-    _pdnn_output.z = 0.02f * (-_e_R.z * 40.0f - _e_Omega.z * 40.0f - 1.0f *_integrator.z - _geomrtry_output.z - 0.0f *_phi_z + 0.0f*Aug.z); //偏航误差e_R.z很容易就趋近于0，会导致无法满足持续激励假设
+    _pdnn_output.x = 0.01f * (-_e_R.x * 40.0f - _e_Omega.x * 40.0f - 0.0f * _integrator.x - _geomrtry_output.x - 0.0f *_phi_x + 0.0f*Aug.x); 
+    _pdnn_output.y = 0.01f * (-_e_R.y * 40.0f - _e_Omega.y * 40.0f - 0.0f *_integrator.y - _geomrtry_output.y- 0.0f * _phi_y + 0.0f*Aug.y);
+    _pdnn_output.z = 0.02f * (-_e_R.z * 40.0f - _e_Omega.z * 40.0f - 0.0f *_integrator.z - _geomrtry_output.z - 0.0f *_phi_z + 0.0f*Aug.z); //偏航误差e_R.z很容易就趋近于0，会导致无法满足持续激励假设
    
    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~L1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  // 把当前输出当作 L1 的 baseline 力矩
@@ -180,7 +180,7 @@ Vector3f AC_PDNN_SO3::update_all(const Matrix3f &R_c, const Matrix3f &R, const V
         //==================== L1 姿态自适应补偿 ====================//
         // L1 参数
         float _a_s_L1   = 40.0f;   // predictor 收敛速度（可调）
-        float _omega_c_L1 = 30.0f; // LPF 带宽 rad/s（可调）
+        float _omega_c_L1 = 10.0f; // LPF 带宽 rad/s（可调）
 
         if (is_positive(dt)) {
             // 1) 预测器：\dot{\hat{\Omega}}
