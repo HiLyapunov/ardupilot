e14bae2039be42bd27b8a77c0788da66205a397e
HiLyapunov <gao.tianhua1103@gmail.com>
2025-11-25 16:29:42 +0900

    impact test 120,120,50 learning rate

--- libraries/AC_PID/AC_PDNN_SO3.cpp
+++ libraries/AC_PID/AC_PDNN_SO3.cpp
@@ -257,7 +257,7 @@ const float Wmax = 500.0f;     // 权重范数上限（球半径）
 //——— X 轴 ———
 {
     // 1) 名义律 dW_nom = gamma * (e_Ω + c_R e_R) * h
-    const float _gamma_x = 80.0f;
+    const float _gamma_x = 120.0f;
     const float z_x = _e_Omega.x + c_R * _e_R.x;
 
     float dW_nom_x1 = _gamma_x * z_x * _h_x_1;
@@ -314,7 +314,7 @@ const float Wmax = 500.0f;     // 权重范数上限（球半径）
 
 //——— Y 轴 ———
 {
-    const float _gamma_y = 80.0f;
+    const float _gamma_y = 120.0f;
     const float z_y = _e_Omega.y + c_R * _e_R.y;
 
     float dW_nom_y1 = _gamma_y * z_y * _h_y_1;
@@ -368,7 +368,7 @@ const float Wmax = 500.0f;     // 权重范数上限（球半径）
 
 //——— Z 轴 ———
 {
-    const float _gamma_z = 30.0f;
+    const float _gamma_z = 50.0f;
     const float z_z = _e_Omega.z + c_R * _e_R.z;
 
     float dW_nom_z1 = _gamma_z * z_z * _h_z_1;
