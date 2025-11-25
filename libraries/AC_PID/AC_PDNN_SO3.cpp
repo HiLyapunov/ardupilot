f88b25ab001d0277555039ad2a33eca59d42f34d
HiLyapunov <gao.tianhua1103@gmail.com>
2025-11-25 14:20:13 +0900

    just save

--- libraries/AC_PID/AC_PDNN_SO3.cpp
+++ libraries/AC_PID/AC_PDNN_SO3.cpp
@@ -433,6 +433,88 @@ const float Wmax = 500.0f;     // 权重范数上限（球半径）
         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~自适应律~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+        float _J_max = 0.03f; //定义自适应参数上限
+        //~~~~~x方向
+        float _eta_x = 0.05f; //定义自适应律参数，eta越大越平滑
+        float _s_x = 0.02f;  //定义缩放因子
+        float c_R = 0.6f;
+        if ((_e_Omega.x + c_R * _e_R.x) * _pdnn_output.x > 0 )
+        {
+            _dot_J_x = -(_J_x * _J_x) / _eta_x * (_e_Omega.x + c_R * _e_R.x) * _pdnn_output.x;
+        }
+        if ((_e_Omega.x + c_R * _e_R.x) * _pdnn_output.x <= 0 )
+        {
+            if (_J_x < _J_max)
+            {
+                _dot_J_x = -(_J_x * _J_x) / _eta_x * (_e_Omega.x + c_R * _e_R.x) * _pdnn_output.x;
+            }
+            else
+            {
+                _dot_J_x = -_s_x * (_J_x * _J_x);  
+            }
+        }
+         //计算当前自适应参数（求积分）
+        if (is_positive(dt)) { //检查时间步长是否有效
+
+        //x方向的自适应参数_m_x
+          _J_x += _dot_J_x * dt;
+        }
+
+        //~~~~~~y方向
+        float _eta_y = 0.05f; //定义自适应律参数，eta越大越平滑
+        float _s_y = 0.02f;  //定义缩放因子
+        if ((_e_Omega.y + c_R * _e_R.y) * _pdnn_output.y > 0 )
+        {
+            _dot_J_y = -(_J_y * _J_y) / _eta_y * (_e_Omega.y + c_R * _e_R.y) * _pdnn_output.y;
+        }
+        if ((_e_Omega.y + c_R * _e_R.y) * _pdnn_output.y <= 0 )
+        {
+            if (_J_y < _J_max)
+            {
+                _dot_J_y = -(_J_y * _J_y) / _eta_y * (_e_Omega.y + c_R * _e_R.y) * _pdnn_output.y;
+            }
+            else
+            {
+                _dot_J_y = -_s_y * (_J_y * _J_y);  
+            }
+        }
+         //计算当前自适应参数（求积分）
+        if (is_positive(dt)) { //检查时间步长是否有效
+
+        //x方向的自适应参数_m_x
+          _J_y += _dot_J_y * dt;
+        }
+
+        //~~~~~~z方向
+        float _J_max_z = 0.04f; //定义自适应参数上限
+        float _eta_z = 0.05f; //定义自适应律参数，eta越大越平滑
+        float _s_z = 0.02f;  //定义缩放因子
+        if ((_e_Omega.z + c_R * _e_R.z) * _pdnn_output.z > 0 )
+        {
+            _dot_J_z = -(_J_z * _J_z) / _eta_z * (_e_Omega.z + c_R * _e_R.z) * _pdnn_output.z;
+        }
+        if ((_e_Omega.z + c_R * _e_R.z) * _pdnn_output.z <= 0 )
+        {
+            if (_J_z < _J_max_z)
+            {
+                _dot_J_z = -(_J_z * _J_z) / _eta_z * (_e_Omega.z + c_R * _e_R.z) * _pdnn_output.z;
+            }
+            else
+            {
+                _dot_J_z = -_s_z * (_J_z * _J_z);  
+            }
+        }
+         //计算当前自适应参数（求积分）
+        if (is_positive(dt)) { //检查时间步长是否有效
+
+        //x方向的自适应参数_m_x
+          _J_z += _dot_J_z * dt;
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    
     _pdnn_output_R.x = -_e_R.x * _kR; //计算P项输出
